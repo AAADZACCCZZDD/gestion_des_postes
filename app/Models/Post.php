46a409bcd498e8aec972b406237d4c1ed9d7a50a
mourad <mourad@gmail.com>
2022-07-18 11:07:59 +0100

    relation ManyToMany (posts and tags table) + tincker: attach, synchWithoutDetaching, Sync, detach

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 
+use App\Models\Tag;
 use App\Models\User;
 use App\Models\Comment;
 use App\Scopes\LatestScope;
@@ -31,6 +32,11 @@ class Post extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function tag()
+    {
+        return $this->belongsToMany(Tag::class)->withTimestamps();
     }
 
     public static function boot()
